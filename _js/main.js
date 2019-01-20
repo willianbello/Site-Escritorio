af4a4406316304096a57a29d2c6872defa592625
willianbello <willianbello.s@gmail.com>
2019-01-20 02:21:26 -0200

    adicionado chat

--- _js/main.js
+++ _js/main.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() {
     
-    var link = $('menu li a');
-    var inicio = $('#inicio');
+    var link = $('.menu li a');
+    var inicio = $('#botaoInicio');
     
     var botaoModal = $('#abrirModal');
     var formulario = $('.formulario');
@@ -27,10 +27,12 @@ $(document).ready(function() {
     });
 
     /* Botão para subir */
-    $(inicio).click(function() {
+    $('#inicio').click(function() {
+        //console.log('botao Clicado');
         $("html, body").animate({
             scrollTop: 0
         }, 700);
+        //console.log('passou pelo função');
     });
    
     /* Fazer surgir quando atingir uma altura */
@@ -38,13 +40,7 @@ $(document).ready(function() {
        
         var minhaPosicao = $(this).scrollTop();
 
-        //console.log(minhaPosicao);
-
-        if (minhaPosicao == 0){
-            $('.logoSite').css('height', '2em');
-        }else{
-            $('.logoSite').css('height', '1.7em');
-        }
+        console.log(minhaPosicao);
 
         if (minhaPosicao >= 200){
             balancaFrase.css('opacity', 100);
@@ -62,8 +58,41 @@ $(document).ready(function() {
             areas.css('opacity', 100);
             areas.addClass('animated');
             areas.addClass('flip-right');
+        }
 
-            /*areas.addClass('flip-bottom');*/
+        if (minhaPosicao == 0){
+            $('.logoSite').css('height', '2em');
+            $('#inicio').addClass('text-focus-in');
+            $('#inicio').addClass('bordaBottom');
+        }else{
+            $('.logoSite').css('height', '1.7em');
+            $('#inicio').removeClass('text-focus-in');
+            $('#inicio').removeClass('bordaBottom');
+        }
+
+        if (minhaPosicao >= 250 && minhaPosicao <= 699){
+            $('#apresentacao').addClass('text-focus-in');
+            $('#apresentacao').addClass('bordaBottom');
+        } else  {
+            $('#apresentacao').removeClass('text-focus-in');
+            $('#apresentacao').removeClass('bordaBottom');
+        }
+
+        if (minhaPosicao >= 700 && minhaPosicao <= 1250){
+            $('#atuacao').addClass('text-focus-in');
+            $('#atuacao').addClass('bordaBottom');
+        } else  {
+            $('#atuacao').removeClass('text-focus-in');
+            $('#atuacao').removeClass('bordaBottom');
+        }
+
+        if (minhaPosicao >= 1251){
+            $('#local').addClass('text-focus-in');
+            $('#local').addClass('bordaBottom');
+            
+        } else { 
+            $('#local').removeClass('text-focus-in');
+            $('#local').removeClass('bordaBottom');
         }
 
         if (minhaPosicao >= 400) {
